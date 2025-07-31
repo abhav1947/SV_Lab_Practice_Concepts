39928e6c4a335745080406a748e14681413684ba
abhav1947 <66078840+abhav1947@users.noreply.github.com>
2025-07-31 22:15:59 +0530

    Add files via upload

---
+++ lab19_enum_type.sv
@@ -0,0 +1,19 @@
+class enum_type;
+  randc enum {ABHAV, VENKAT, VELIDI, APPU} _state_req;
+  
+  function void display_s();
+    $display("_state_req1=%s",_state_req.name());
+  endfunction
+endclass
+
+module tb;
+  enum_type e1;
+  
+  initial begin
+    e1 = new();
+    repeat(10) begin
+      e1.randomize();
+      e1.display_s();
+    end
+  end
+endmodule
