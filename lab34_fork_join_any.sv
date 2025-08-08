f9bab24bcb17a598c5c04fd18e78ac4aff69464d
abhav1947 <66078840+abhav1947@users.noreply.github.com>
2025-08-08 23:46:46 +0530

    Create lab34_fork_join_any.sv

---
+++ lab34_fork_join_any.sv
@@ -0,0 +1,23 @@
+module tb;
+  initial begin
+	#1;
+    $display("[st-1] Print at %0t",$time);
+    fork
+      begin
+        #3;
+        $display("[st-2] Print at %0t",$time);
+        #5;
+        $display("[st-3] Print at %0t",$time);
+      end
+      
+      begin
+        #2;
+        $display("[st-4] Print at %0t",$time);
+      end
+      
+      $display("[st-5] Print at %0t",$time);
+      
+    join_any
+    $display("[st-6] Print at %0t",$time);
+  end
+endmodule
