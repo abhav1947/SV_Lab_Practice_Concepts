f0cb5d29f88a1ca142aacd6e2a5e2bd4f0da4ec5
abhav1947 <66078840+abhav1947@users.noreply.github.com>
2025-12-26 23:21:24 +0530

    Create lab_37_events_blocking.sv

---
+++ lab_37_events_blocking.sv
@@ -0,0 +1,26 @@
+// Event e1 has triggered and wait has occured after it
+module tb;
+  
+  event e1;
+  
+  task process_A();
+    $display("Process A has started at %0t", $time);
+    #10;
+    -> e1;
+    $display("Process A has ended at %0t", $time);
+  endtask
+  
+  task process_B();
+    $display("Process B has started at %0t", $time);
+    #11;
+    @(e1.triggered);
+    $display("Process B has ended at %0t", $time);
+  endtask    
+  
+  initial begin
+    fork 
+      process_A();
+      process_B();
+    join
+  end
+endmodule
