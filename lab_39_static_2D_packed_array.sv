2b2fa2e96c838b26fc5e410202f36c86c67eaaaf
abhav1947 <66078840+abhav1947@users.noreply.github.com>
2026-01-06 17:30:42 +0530

    Create lab_39_static_2D_packed_array.sv

---
+++ lab_39_static_2D_packed_array.sv
@@ -0,0 +1,13 @@
+module tb;
+  bit [5:0] array[6][2] = '{'{1, 100}, '{2, 200}, '{3, 300}, {4, 400}, '{5, 500},  '{6, 600}};
+  
+  initial begin 
+    foreach(array[i,j]) begin
+      array[i][j] = $urandom;
+    end
+    foreach(array[i,j]) begin
+      $display("array[%0d][%0d] = %0d", i, j, array[i][j]);
+    end
+  end
+  
+endmodule
